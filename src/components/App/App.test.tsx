69d13fd57984d923c14d2d129cf57598a4de95e8
fabien0102 <fabien0102@planee.fr>
2017-01-04 20:03:54 +0100

    Add snapshot to test app

--- src/components/App/App.test.tsx
+++ src/components/App/App.test.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import { shallow } from 'enzyme';
+import * as renderer from 'react-test-renderer';
 import { App } from './App';
 import { initialState } from '../../reducers/game.reducer';
 import 'jest';
@@ -9,5 +10,11 @@ describe('App', () => {
         const app = shallow(<App game={initialState} dispatch={null} />);
 
         expect(app.find('h1').text()).toEqual('Connect 4 react');
+    });
+
+    it('should render correctly', () => {
+        const tree = renderer.create(<App game={initialState} dispatch={null} />).toJSON();
+
+        expect(tree).toMatchSnapshot();
     });
 });
